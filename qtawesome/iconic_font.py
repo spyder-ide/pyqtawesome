c5f2255bd475a07c873d77273cffad6a9d2680d8
dalthviz <16781833+dalthviz@users.noreply.github.com>
2025-02-11 19:46:29 -0500

    Add fallback when loading fonts to try to load them from the system main fonts directory

--- qtawesome/iconic_font.py
+++ qtawesome/iconic_font.py
@@ -379,6 +379,15 @@ class IconicFont(QObject):
                 id_ = QFontDatabase.addApplicationFontFromData(data)
             font_data.close()
 
+            if id_ == -1 and os.name == "nt":
+                # Try to load font from system Fonts directory
+                windows_dir = os.environ.get("WINDIR")
+                if windows_dir:
+                    from_system_ttf = os.path.join(
+                        windows_dir, "Fonts", ttf_filename
+                    )
+                    id_ = QFontDatabase.addApplicationFont(from_system_ttf)
+
             loadedFontFamilies = QFontDatabase.applicationFontFamilies(id_)
 
             if loadedFontFamilies:
