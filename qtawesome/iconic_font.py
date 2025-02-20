f9e80962735031336a57ef7d5c65818e14979d02
dalthviz <16781833+dalthviz@users.noreply.github.com>
2025-02-20 17:39:19 -0500

    Add warning message with more info and expand comment with reasoning

--- qtawesome/iconic_font.py
+++ qtawesome/iconic_font.py
@@ -730,11 +730,21 @@ class IconicFont(QObject):
                         # since the load failed we need to return here where
                         # the font file is originally located. With that a
                         # `FontError` can be eventually raised with useful
-                        # information (i.e not being able to load bundled fonts
-                        # pointing that the fonts are located on the QtAwesome
-                        # `fonts` directory and manual configuration is needed
-                        # to allow them).
+                        # information if necessary (i.e not being able to load
+                        # bundled fonts pointing that the fonts are located on
+                        # the QtAwesome `fonts` directory and manual
+                        # configuration is needed to allow them). Also, if we
+                        # don't detected the font being available from the
+                        # system fonts directory a warning with a more specific
+                        # suggestion (install the font manually) is shown.
                         # See spyder-ide/qtawesome#264
+                        if not os.path.isfile(os.path.join(system_fonts_dir, filename)):
+                            warnings.warn(
+                                "Font at '{0}' could not be loaded. If possible, "
+                                "install it system wide to make it available. "
+                                "Font source file to be manually installed can "
+                                "be found at '{1}'".format(dst_path, src_path)
+                            )
                         return fonts_directory
 
                     # Store the fontname/filename in the registry
